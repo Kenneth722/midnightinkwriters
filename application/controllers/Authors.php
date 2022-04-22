66c940a6d44cce812775e769f46509cfd7058d8d
Kenneth <kenneth.rygeltech@gmail.com>
2022-04-23 01:56:17 +0800

    Update Addnewview Method By adding subscription amount and type get Request to a Variable Data

--- application/controllers/Authors.php
+++ application/controllers/Authors.php
@@ -7,35 +7,64 @@
     		$this->load->library(['ion_auth', 'form_validation']);
     	}
     	public function addNewView() {
-        	$this->load->view('authors/index');
+    		$data['subscription_price'] = $this->input->get('price');
+    		$data['subscription_type'] = $this->input->get('type');
+
+    		$this->load->view('templates/header');
+        	$this->load->view('authors/index', $data);
+        	$this->load->view('templates/footer');
         }
 
         public function addNew() {
-        	$fname = $this->input->post('f_name');
-        	$mname = $this->input->post('m_name');
-        	$lname = $this->input->post('l_name');
-        	$email = $this->input->post('email');
-        	$username = $fname . ' ' . $mname . ' ' . $lname;
-        	$password = $this->input->post('password');
-        	$address = $this->input->post('address');
-        	$additional_data = array(
-                'first_name' => $fname,
-                'last_name' => $lname,
-                );
-        	$dfg = 3;
+        	require_once(APPPATH.'libraries/payment/stripe-php-master/init.php');
 
-        	$this->ion_auth->register($username, $password, $email, $dfg);
-        	$ion_user_id = $this->db->get_where('users', array('email' => $email))->row()->id;
-        	$data = array(
-        		'firstname' => $fname,
-        		'middlename' => $mname,
-        		'lastname' => $lname,
-        		'address' => $address,
-        	);
-            $this->author_model->insertAuthor($data);
-            // $author_user_id = $this->db->get_where('authors', array('email' => $email))->row()->id;
-            $inserted_id = $this->db->insert_id();
-            $id_info = array('ion_user_id' => $ion_user_id);
-            $this->author_model->updateAuthor($inserted_id, $id_info);
+        	$stripeDetails = array(
+		        "secretKey" => "sk_test_51Ko7tbDDDYQjuAYT90m8u05TtJH3beFZB20RTOwDhfWjkOeZkZrKAuyQAyL6Cz2Wyxd2YJdmlRYnSsTTlcWzNcqg005QUnOirv",
+		        "publishableKey" => "pk_test_51Ko7tbDDDYQjuAYT9RSleqcLG4Lm9Rdu3AkLXAc6V269sK8D5zY7m79lcythrbZQ3mWAuCRMP09fqfAhSoGAMCAD007tOgi0D4"
+		    );
+
+		    \Stripe\Stripe::setApiKey($stripeDetails["secretKey"]);
+
+		    $token = $_POST["stripeToken"];
+		    $sub_amount = $this->input->post('sub_price');
+		    $sub_type = $this->input->post('sub_type');
+		    $desc = "Author".' '.$sub_type.' '."Subscription";
+		    $charge = \Stripe\Charge::create([
+		      "amount" => str_replace(",","",$sub_amount) * 100,
+		      "currency" => 'usd',
+		      "description"=>$desc,
+		      "source"=> $token,
+		    ]);
+
+		    if($charge){
+		      redirect('authors/addNewView');
+		    }
+
+        	// $fname = $this->input->post('f_name');
+        	// $mname = $this->input->post('m_name');
+        	// $lname = $this->input->post('l_name');
+        	// $email = $this->input->post('email');
+        	// $username = $fname . ' ' . $mname . ' ' . $lname;
+        	// $password = $this->input->post('password');
+        	// $address = $this->input->post('address');
+        	// $additional_data = array(
+         //        'first_name' => $fname,
+         //        'last_name' => $lname,
+         //        );
+        	// $dfg = 3;
+
+        	// $this->ion_auth->register($username, $password, $email, $dfg);
+        	// $ion_user_id = $this->db->get_where('users', array('email' => $email))->row()->id;
+        	// $data = array(
+        	// 	'firstname' => $fname,
+        	// 	'middlename' => $mname,
+        	// 	'lastname' => $lname,
+        	// 	'address' => $address,
+        	// );
+         //    $this->author_model->insertAuthor($data);
+         //    // $author_user_id = $this->db->get_where('authors', array('email' => $email))->row()->id;
+         //    $inserted_id = $this->db->insert_id();
+         //    $id_info = array('ion_user_id' => $ion_user_id);
+         //    $this->author_model->updateAuthor($inserted_id, $id_info);
         }
     }
