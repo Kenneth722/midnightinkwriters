5e6fcf987dd6cf83aa9569b77fe5a94e4eb07fe0
Kenneth <kenneth.rygeltech@gmail.com>
2022-04-23 01:54:19 +0800

    Add Stripe Payment Library and add Subscription Index View

---
+++ application/views/subscription/index.php
@@ -0,0 +1,30 @@
+<div id="site-content" style="padding-top: 102px">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12 col-sm-12">
+				<div class="card bg-purple">
+					<div class="card-body">
+						<center class="text-white">
+							<p class="h1"><strong>Basic</strong></p>
+							<a href="authors/addnewview?price=1000&type=basic" class="btn btn-success">Subscribe</a>
+						</center>
+					</div>
+				</div>
+			</div>
+			<!-- <div class="col-md-4 col-sm-12">
+				<div class="card">
+					<div class="card-body">
+						
+					</div>
+				</div>
+			</div>
+			<div class="col-md-4 col-sm-12">
+				<div class="card">
+					<div class="card-body">
+						
+					</div>
+				</div>
+			</div> -->
+		</div>
+	</div>
+</div>
