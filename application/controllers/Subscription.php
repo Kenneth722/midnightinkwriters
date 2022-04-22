b587e04de4b120fbb845fe36578808fbff1b6adf
Kenneth <kenneth.rygeltech@gmail.com>
2022-04-23 01:57:04 +0800

    Update Subscription Controller by adding index Method

--- application/controllers/Subscription.php
+++ application/controllers/Subscription.php
@@ -1,4 +1,8 @@
 <?php
     class Subscription extends CI_Controller {
-    	
+    	public function index() {
+    		$this->load->view('templates/header');
+    		$this->load->view('subscription/index');
+    		$this->load->view('templates/footer');
+    	}
     }
