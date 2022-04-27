266e830eea7b60ab26d9cc9047212e506838652d
Kenneth <kenneth.rygeltech@gmail.com>
2022-04-28 02:15:18 +0800

    Update Login by adding User Friendly Design

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -1,28 +1,46 @@
-<h1><?php echo lang('login_heading');?></h1>
-<p><?php echo lang('login_subheading');?></p>
+<div id="site-content">
+  <div class="container">
+    <h1><?php echo lang('login_heading');?></h1>
+    <p><?php echo lang('login_subheading');?></p>
 
-<div id="infoMessage"><?php echo $message;?></div>
-
-<?php echo form_open("auth/login");?>
-
-  <p>
-    <?php echo lang('login_identity_label', 'identity');?>
-    <?php echo form_input($identity);?>
-  </p>
-
-  <p>
-    <?php echo lang('login_password_label', 'password');?>
-    <?php echo form_input($password);?>
-  </p>
-
-  <p>
-    <?php echo lang('login_remember_label', 'remember');?>
-    <?php echo form_checkbox('remember', '1', FALSE, 'id="remember"');?>
-  </p>
+    <div id="infoMessage"><?php echo $message;?></div>
 
 
-  <p><?php echo form_submit('submit', lang('login_submit_btn'));?></p>
+    <div class="row">
+      <div class="col-md-6 col-sm-6">
+        <div class="card">
+          <div class="card-body">
+            <?php echo form_open("auth/login");?>
+              <p>
+                <?php echo lang('login_identity_label', 'identity');?>
+                <?php echo form_input($identity);?>
+              </p>
 
-<?php echo form_close();?>
+              <p>
+                <?php echo lang('login_password_label', 'password');?>
+                <?php echo form_input($password);?>
+              </p>
 
-<p><a href="forgot_password"><?php echo lang('login_forgot_password');?></a></p>
+              <p>
+                <?php echo lang('login_remember_label', 'remember');?>
+                <?php echo form_checkbox('remember', '1', FALSE, 'id="remember"');?>
+              </p>
+
+              <p><?php echo form_submit($submit);?></p>
+            <?php echo form_close();?>
+            <p><a href="forgot_password"><?php echo lang('login_forgot_password');?></a></p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6 col-sm-6">
+        <section id="hero" class="p-0">
+          <div class="hero-img">
+            <center>
+              <img src="<?php echo base_url(); ?>/assets/img/homepagebook.jpg" class="img-fluid animated" alt="">
+            </center>
+          </div>
+        </section>
+      </div>
+    </div>
+  </div>
+</div>
