384d7323669a8297046842444b033cb157fa844d
Kenneth <kenneth.rygeltech@gmail.com>
2022-04-27 08:34:40 +0800

    Update Header by Grouping Header Content for ADMIN and AUTHOR

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -48,64 +48,84 @@
         <!-- <a href="index.html" class="logo"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
 
         <nav id="navbar" class="navbar">
-            <ul>
-            <li><a class="nav-link scrollto active" href="<?php echo base_url(); ?>">Home</a></li>
-            <li><a class="nav-link scrollto" href="#about">Pay</a></li>
-            <li><a class="nav-link scrollto" href="#services">Fanbase Boost</a></li>
-            <li><a class="nav-link scrollto" href="#portfolio">Local Listing SEO</a></li>
-            <li class="dropdown"><a href="#"><span>Publishing</span> <i class="bi bi-chevron-down"></i></a>
+            <?php if (!$this->ion_auth->logged_in()) { ?>
                 <ul>
-                <li><a href="#">Drop Down 1</a></li>
-                <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
-                    <ul>
-                    <li><a href="#">Deep Drop Down 1</a></li>
-                    <li><a href="#">Deep Drop Down 2</a></li>
-                    <li><a href="#">Deep Drop Down 3</a></li>
-                    <li><a href="#">Deep Drop Down 4</a></li>
-                    <li><a href="#">Deep Drop Down 5</a></li>
-                    </ul>
-                </li>
-                <li><a href="#">Drop Down 2</a></li>
-                <li><a href="#">Drop Down 3</a></li>
-                <li><a href="#">Drop Down 4</a></li>
+                    <li><a class="nav-link scrollto active" href="<?php echo base_url(); ?>">Home</a></li>
+                    <li><a class="nav-link scrollto" href="#about">Pay</a></li>
+                    <li><a class="nav-link scrollto" href="#services">Fanbase Boost</a></li>
+                    <li><a class="nav-link scrollto" href="#portfolio">Local Listing SEO</a></li>
+                    <li class="dropdown"><a href="#"><span>Publishing</span> <i class="bi bi-chevron-down"></i></a>
+                        <ul>
+                        <li><a href="#">Drop Down 1</a></li>
+                        <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
+                            <ul>
+                            <li><a href="#">Deep Drop Down 1</a></li>
+                            <li><a href="#">Deep Drop Down 2</a></li>
+                            <li><a href="#">Deep Drop Down 3</a></li>
+                            <li><a href="#">Deep Drop Down 4</a></li>
+                            <li><a href="#">Deep Drop Down 5</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">Drop Down 2</a></li>
+                        <li><a href="#">Drop Down 3</a></li>
+                        <li><a href="#">Drop Down 4</a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown"><a href="#"><span>Bookstore Placement</span> <i class="bi bi-chevron-down"></i></a>
+                        <ul>
+                        <li><a href="#">Drop Down 1</a></li>
+                        <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
+                            <ul>
+                            <li><a href="#">Deep Drop Down 1</a></li>
+                            <li><a href="#">Deep Drop Down 2</a></li>
+                            <li><a href="#">Deep Drop Down 3</a></li>
+                            <li><a href="#">Deep Drop Down 4</a></li>
+                            <li><a href="#">Deep Drop Down 5</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">Drop Down 2</a></li>
+                        <li><a href="#">Drop Down 3</a></li>
+                        <li><a href="#">Drop Down 4</a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown"><a href="#"><span>More</span> <i class="bi bi-chevron-down"></i></a>
+                        <ul>
+                        <li><a href="#">Drop Down 1</a></li>
+                        <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
+                            <ul>
+                            <li><a href="#">Deep Drop Down 1</a></li>
+                            <li><a href="#">Deep Drop Down 2</a></li>
+                            <li><a href="#">Deep Drop Down 3</a></li>
+                            <li><a href="#">Deep Drop Down 4</a></li>
+                            <li><a href="#">Deep Drop Down 5</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">Drop Down 2</a></li>
+                        <li><a href="#">Drop Down 3</a></li>
+                        <li><a href="#">Drop Down 4</a></li>
+                        </ul>
+                    </li>
                 </ul>
-            </li>
-            <li class="dropdown"><a href="#"><span>Bookstore Placement</span> <i class="bi bi-chevron-down"></i></a>
+                <i class="bi bi-list mobile-nav-toggle"></i>
+            <?php } else { ?>
                 <ul>
-                <li><a href="#">Drop Down 1</a></li>
-                <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
-                    <ul>
-                    <li><a href="#">Deep Drop Down 1</a></li>
-                    <li><a href="#">Deep Drop Down 2</a></li>
-                    <li><a href="#">Deep Drop Down 3</a></li>
-                    <li><a href="#">Deep Drop Down 4</a></li>
-                    <li><a href="#">Deep Drop Down 5</a></li>
-                    </ul>
-                </li>
-                <li><a href="#">Drop Down 2</a></li>
-                <li><a href="#">Drop Down 3</a></li>
-                <li><a href="#">Drop Down 4</a></li>
+                    <?php if ($this->ion_auth->in_group(array('admin'))) { ?>
+                        <li><a class="nav-link scrollto active" href="<?php echo base_url(); ?>">Home</a></li>
+                        <li><a class="nav-link scrollto" href="#services">Services</a></li>
+                    <?php } ?>
+                    <?php if ($this->ion_auth->in_group(array('author'))) { ?>
+                        <li><a class="nav-link scrollto active" href="<?php echo base_url(); ?>">Home</a></li>
+                        <li><a class="nav-link scrollto" href="#services">Services</a></li>
+                        <li class="dropdown"><a href="#"><span>Profile</span> <i class="bi bi-chevron-down"></i></a>
+                            <ul>
+                            <li><a href="#">My Info</a></li>
+                            <li><a href="auth/logout">Logout</a></li>
+                            </ul>
+                        </li>
+                    <?php } ?>
                 </ul>
-            </li>
-            <li class="dropdown"><a href="#"><span>More</span> <i class="bi bi-chevron-down"></i></a>
-                <ul>
-                <li><a href="#">Drop Down 1</a></li>
-                <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
-                    <ul>
-                    <li><a href="#">Deep Drop Down 1</a></li>
-                    <li><a href="#">Deep Drop Down 2</a></li>
-                    <li><a href="#">Deep Drop Down 3</a></li>
-                    <li><a href="#">Deep Drop Down 4</a></li>
-                    <li><a href="#">Deep Drop Down 5</a></li>
-                    </ul>
-                </li>
-                <li><a href="#">Drop Down 2</a></li>
-                <li><a href="#">Drop Down 3</a></li>
-                <li><a href="#">Drop Down 4</a></li>
-                </ul>
-            </li>
-            </ul>
-            <i class="bi bi-list mobile-nav-toggle"></i>
+                <i class="bi bi-list mobile-nav-toggle"></i>
+            <?php } ?>
         </nav><!-- .navbar -->
 
         </div>
