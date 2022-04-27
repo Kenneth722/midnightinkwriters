8da1675243f1b1de6a9eebaea18b8e1e97c909d5
kenneth <kenneth.rygeltech@gmail.com>
2022-04-27 16:36:05 +0800

    Update Subscription COntroller by adding __construction Method with initialize as Parent Method and load Library of ion_auth

--- application/controllers/Subscription.php
+++ application/controllers/Subscription.php
@@ -1,5 +1,10 @@
 <?php
     class Subscription extends CI_Controller {
+        public function __construct() {
+            parent::__construct();
+            $this->load->library(['ion_auth', 'form_validation']);
+        }
+        
     	public function index() {
     		$this->load->view('templates/header');
     		$this->load->view('subscription/index');
