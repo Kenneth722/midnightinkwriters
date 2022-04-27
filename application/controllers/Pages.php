cc17bc3256c95e951281232033d8c5645948acee
Kenneth <kenneth.rygeltech@gmail.com>
2022-04-27 08:29:27 +0800

    Update Pages Controller by adding Construct as a Parent Method and Load ion auth Library

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -1,5 +1,10 @@
 <?php
     class Pages extends CI_Controller{
+        public function __construct() {
+            parent::__construct();
+            $this->load->library(['ion_auth', 'form_validation']);
+        }
+
         public function view($page = 'home'){
             if (!file_exists(APPPATH.'views/pages/'.$page.'.php')) {
                 show_404();
