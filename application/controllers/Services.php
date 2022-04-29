9ad781200a160f28662ae9fefea4d47adff2eade
Kenneth <kenneth.rygeltech@gmail.com>
2022-04-30 01:13:20 +0800

    Update Services Controller by Loading Service Model and add Insert to Table Process on addNew method

--- application/controllers/Services.php
+++ application/controllers/Services.php
@@ -3,6 +3,7 @@
         public function __construct() {
         	parent::__construct();
         	$this->load->library(['ion_auth', 'form_validation']);
+        	$this->load->model('service_model');
         }
 
         public function index() {
@@ -25,9 +26,27 @@
 
         	$name = $this->input->post('name');
         	$price = $this->input->post('price');
-        	$category = $this->input->post('category');
         	$description = $this->input->post('description');
+        	$sub_description = $this->input->post('sub_description');
+        	$receive_description = $this->input->post('receive_description');
+        	$use_description = $this->input->post('use_description');
+        	$display_name = str_replace(' ', '_', $name);
+        	$date = gmdate('Y-m-d H:i:s');
 
+        	$data = array(
+        		"name" => $name,
+        		"display_name" => $display_name,
+        		"description" => $description,
+        		"sub_description" => $sub_description,
+        		"receive_description" => $receive_description,
+        		"use_description" => $use_description,
+        		"price" => $price,
+        		"created_at" => $date
+        	);
+
+        	$this->service_model->insertService($data);
+
+        	redirect('/');
 
         }
     }
