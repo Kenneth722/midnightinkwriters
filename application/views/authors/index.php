3c3ce7e00609e00798457ab87d5a552a4029e7e8
Kenneth <kenneth.rygeltech@gmail.com>
2022-05-02 09:21:58 +0800

    Update Author Index and Service Index by adding Table and Stack it with Datas from Controller

--- application/views/authors/index.php
+++ application/views/authors/index.php
@@ -1,73 +1,39 @@
-<div id="site-content" style="padding-top: 102px">
+<div id="site-content">
 	<div class="container">
-		<div class="row mt-5">
+		<div class="row">
 			<div class="col-md-12 col-sm-12">
 				<div class="card">
-					<div class="card-header">
-						<div class="card-title">Register Author</div>
-					</div>
-					<form method="POST" action="authors/addNew">
-						<div class="card-body">
-							<div class="row">
-								<div class="col-md-6 col-sm-12">
-									<div class="form-group">
-										<label class="form-label">First Name</label>
-										<input type="text" name="f_name" class="form-control" required>
-									</div>
-								</div>
-								<div class="col-md-6 col-sm-12">
-									<div class="form-group">
-										<label class="form-label">Middle Name</label>
-										<input type="text" name="m_name" class="form-control">
-									</div>
-								</div>
-							</div>
-							<div class="row">
-								<div class="col-md-6 col-sm-12">
-									<div class="form-group">
-										<label class="form-label">Last Name</label>
-										<input type="text" name="l_name" class="form-control" required>
-									</div>
-								</div>
-								<div class="col-md-6 col-sm-12">
-									<div class="form-group">
-										<label class="form-label">Email</label>
-										<input type="text" name="email" class="form-control" required>
-									</div>
-								</div>
-							</div>
-							<div class="row">
-								<div class="col-md-6 col-sm-12">
-									<div class="form-group">
-										<label class="form-label">Password</label>
-										<input type="password" name="password" class="form-control" required>
-									</div>
-								</div>
-								<div class="col-md-6 col-sm-12">
-									<div class="form-group">
-										<label class="form-label">Address</label>
-										<input type="text" name="address" class="form-control" required>
-									</div>
-								</div>
-							</div>
-							<div class="row">
-								<input type="hidden" name="sub_price" value="<?php echo $subscription_price ?>">
-								<input type="hidden" name="sub_type" value="<?php echo $subscription_type ?>">
-							</div>
-							<div class="row mt-3">
-								<div class="col-md-12 col-sm-12">
-									<div class="form-group">
-										<script
-						                src="https://checkout.stripe.com/checkout.js" class="stripe-button"
-						                data-key="pk_test_51Ko7tbDDDYQjuAYT9RSleqcLG4Lm9Rdu3AkLXAc6V269sK8D5zY7m79lcythrbZQ3mWAuCRMP09fqfAhSoGAMCAD007tOgi0D4"
-						                data-locale="auto"
-						                data-label="Proceed to Card Payment">
-						                </script>
-									</div>
-								</div>
-							</div>
+					<div class="card-header bg-purple text-white">
+						<div class="card-title">
+							<h4 class="mt-1">List of Authors</h4>
 						</div>
-					</form>
+					</div>
+					<div class="card-body">
+						<table class="table table-responsive" id="editable-1">
+							<thead>
+								<tr>
+									<th>id</th>
+									<th>Name</th>
+									<th>Email</th>
+									<th>Address</th>
+									<th>Actions</th>
+								</tr>
+							</thead>
+							<tbody>
+								<?php foreach($authors as $author) { 
+									$name = $author->firstname . ' ' . $author->middlename . ' ' . $author->lastname;
+								?>
+									<tr>
+										<td><?php echo $author->id ?></td>
+										<td><?php echo $name ?></td>
+										<td><?php echo $this->author_model->getAuthorbyIonId($author->ion_user_id)->email ?></td>
+										<td><?php echo $author->address ?></td>
+										<td></td>
+									</tr>
+								<?php } ?>
+							</tbody>
+						</table>
+					</div>
 				</div>
 			</div>
 		</div>
--- application/views/services/index.php
+++ application/views/services/index.php
@@ -1,6 +1,37 @@
-<!-- <div id="site-content">
-	<p>Services</p>
-</div> -->
-<?php
-	echo phpinfo();
-?>
+<div id="site-content">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12 col-sm-12">
+				<div class="card">
+					<div class="card-header bg-purple text-white">
+						<div class="card-title">
+							<h4 class="mt-1">Services List</h4>
+						</div>
+					</div>
+					<div class="card-body">
+						<table class="table table-responsive" id="editable-1">
+							<thead>
+								<tr>
+									<th>ID</th>
+									<th>Name</th>
+									<th>Price</th>
+									<th>Actions</th>
+								</tr>
+							</thead>
+							<tbody>
+								<?php foreach($services as $service) { ?>
+									<tr>
+										<td><?php echo $service->id ?></td>
+										<td><?php echo $service->name ?></td>
+										<td><?php echo $service->price ?></td>
+										<td></td>
+									</tr>
+								<?php } ?>
+							</tbody>
+						</table>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
