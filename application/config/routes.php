269b64230cdb9aebe37f8f3a328222c6148368c3
Kenneth <kenneth.rygeltech@gmail.com>
2022-05-04 08:57:51 +0800

    Update Route by adding Product Index

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,6 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+$route['products'] = 'products/index';
 $route['authors'] = 'authors/index';
 $route['services'] = 'services/index';
 $route['subscription'] = 'subscription/index';
