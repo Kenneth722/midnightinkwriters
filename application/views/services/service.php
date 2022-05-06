1a64f7fce1b15d6414af3d9af38862f1c8a90703
Kenneth <kenneth.rygeltech@gmail.com>
2022-05-06 09:14:25 +0800

    Add Service View Page and default Image for FanBaseBoost

---
+++ application/views/services/service.php
@@ -0,0 +1,85 @@
+<div id="site-content">
+	<div class="container">
+		<div class="row mt-5">
+			<div class="col-md-8 col-sm-12">
+				<h1 class="text-dark"><strong><?php echo $service->name ?></strong></h1>
+				<img class="mt-5" src="assets/img/fanbaseboost.jpg" alt="" width="800px" height="450px" style="border-radius: 2%">
+				<p class="text-muted mt-3" style="margin-right: 3rem;"><?php echo $service->sub_description ?></p>
+			</div>
+			<div class="col-md-4 col-sm-12">
+				<p class="text-dark h5"><strong>Description</strong></p>
+				<p class="text-muted"><?php echo $service->description ?></p>
+				<hr class="mt-5 mb-5">
+				<div id="paypal-button-container"></div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<script src="https://www.paypal.com/sdk/js?client-id=AUIQuS9BPZThHVpJUFc3aMTkkcNZMzK3Dmz3GgL7fqx5A3vrjFDKYlveQQfGqHFKGHvOKQfk_D4iquBF&currency=USD&intent=capture"></script>
+
+<script>
+  const fundingSources = [
+    paypal.FUNDING.PAYPAL,
+      paypal.FUNDING.VENMO,
+      paypal.FUNDING.PAYLATER,
+      paypal.FUNDING.CARD
+    ]
+
+  for (const fundingSource of fundingSources) {
+    const paypalButtonsComponent = paypal.Buttons({
+      fundingSource: fundingSource,
+
+      // optional styling for buttons
+      // https://developer.paypal.com/docs/checkout/standard/customize/buttons-style-guide/
+      style: {
+        shape: 'pill',
+        height: 40,
+      },
+
+      // set up the transaction
+      createOrder: (data, actions) => {
+        // pass in any options from the v2 orders create call:
+        // https://developer.paypal.com/api/orders/v2/#orders-create-request-body
+        const createOrderPayload = {
+          purchase_units: [
+            {
+              amount: {
+                value: '88.44',
+              },
+            },
+          ],
+        }
+
+        return actions.order.create(createOrderPayload)
+      },
+
+      // finalize the transaction
+      onApprove: (data, actions) => {
+        const captureOrderHandler = (details) => {
+          const payerName = details.payer.name.given_name
+          console.log('Transaction completed!')
+        }
+
+        return actions.order.capture().then(captureOrderHandler)
+      },
+
+      // handle unrecoverable errors
+      onError: (err) => {
+        console.error(
+          'An error prevented the buyer from checking out with PayPal',
+        )
+      },
+    })
+
+    if (paypalButtonsComponent.isEligible()) {
+      paypalButtonsComponent
+        .render('#paypal-button-container')
+        .catch((err) => {
+          console.error('PayPal Buttons failed to render')
+        })
+    } else {
+      console.log('The funding source is ineligible')
+    }
+  }
+</script>
