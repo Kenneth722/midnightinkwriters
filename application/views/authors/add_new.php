407a1fa94e9675aac38d1800b9260573dae893b8
Kenneth <kenneth.rygeltech@gmail.com>
2022-05-11 01:36:11 +0800

    Update ADd New Author Page by adding Id on Each Input Field and add Ajax to target my field to Author Controllers InsertAuthorByJson Method to be save in database after paying on paypal

--- application/views/authors/add_new.php
+++ application/views/authors/add_new.php
@@ -12,13 +12,13 @@
 								<div class="col-md-6 col-sm-12">
 									<div class="form-group">
 										<label class="form-label">First Name</label>
-										<input type="text" name="f_name" class="form-control" required>
+										<input type="text" name="f_name" id="f_name" class="form-control" required>
 									</div>
 								</div>
 								<div class="col-md-6 col-sm-12">
 									<div class="form-group">
 										<label class="form-label">Middle Name</label>
-										<input type="text" name="m_name" class="form-control">
+										<input type="text" name="m_name" id="m_name" class="form-control">
 									</div>
 								</div>
 							</div>
@@ -26,13 +26,13 @@
 								<div class="col-md-6 col-sm-12">
 									<div class="form-group">
 										<label class="form-label">Last Name</label>
-										<input type="text" name="l_name" class="form-control" required>
+										<input type="text" name="l_name" id="l_name" class="form-control" required>
 									</div>
 								</div>
 								<div class="col-md-6 col-sm-12">
 									<div class="form-group">
 										<label class="form-label">Email</label>
-										<input type="text" name="email" class="form-control" required>
+										<input type="text" name="email" id="email" class="form-control" required>
 									</div>
 								</div>
 							</div>
@@ -40,13 +40,13 @@
 								<div class="col-md-6 col-sm-12">
 									<div class="form-group">
 										<label class="form-label">Password</label>
-										<input type="password" name="password" class="form-control" required>
+										<input type="password" name="password" id="password" class="form-control" required>
 									</div>
 								</div>
 								<div class="col-md-6 col-sm-12">
 									<div class="form-group">
 										<label class="form-label">Address</label>
-										<input type="text" name="address" class="form-control" required>
+										<input type="text" name="address" id="address" class="form-control" required>
 									</div>
 								</div>
 							</div>
@@ -97,31 +97,6 @@
   </div>
 </div>
 
-<script src="https://www.paypal.com/sdk/js?client-id=AUIQuS9BPZThHVpJUFc3aMTkkcNZMzK3Dmz3GgL7fqx5A3vrjFDKYlveQQfGqHFKGHvOKQfk_D4iquBF&currency=USD&intent=capture"></script>
-
-<script type="text/javascript">
-	var description = "Subscription";
-  var price = "<?php echo $subscription_price ?>";
-
-	paypal.Buttons({
-      createOrder: function (data, actions) {
-          return actions.order.create({
-              purchase_units: [{
-                  description: description,
-                  amount: {
-                      value: price
-                  }
-              }]
-          });
-      },
-      onApprove: function (data, actions) {
-          return actions.order.capture().then(function (details) {
-              alert('Transaction completed by ' + details.payer.name.given_name);
-          });
-      }
-  }).render('#paypal-button-container');
-</script>
-
 <!-- <script type="text/javascript">
     var description = "Subscription";
     var price = "<?php echo $subscription_price ?>";
@@ -162,3 +137,74 @@
 	    }
 	});
 </script> -->
+	
+	<a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+    <!-- Vendor JS Files -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="<?php echo base_url(); ?>/assets/vendor/purecounter/purecounter.js"></script>
+    <script src="<?php echo base_url(); ?>/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="<?php echo base_url(); ?>/assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="<?php echo base_url(); ?>/assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="<?php echo base_url(); ?>/assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="<?php echo base_url(); ?>/assets/vendor/php-email-form/validate.js"></script>
+    <script type="text/javascript" src="<?php echo base_url(); ?>/assets/plugin/DataTables/datatables.min.js"></script>
+
+    <!-- Template Main JS File -->
+    <script src="<?php echo base_url(); ?>/assets/js/main.js"></script>
+
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $(".subscriptionForm").on("click", ".pay-subscription", function () {
+                $('#exampleModal').modal('show');
+            })
+        })
+    </script>
+
+    <script src="https://www.paypal.com/sdk/js?client-id=AUIQuS9BPZThHVpJUFc3aMTkkcNZMzK3Dmz3GgL7fqx5A3vrjFDKYlveQQfGqHFKGHvOKQfk_D4iquBF&currency=USD&intent=capture"></script>
+
+	<script type="text/javascript">
+		var description = "Subscription";
+	  	var price = "<?php echo $subscription_price ?>";
+
+		paypal.Buttons({
+	      createOrder: function (data, actions) {
+	          return actions.order.create({
+	              purchase_units: [{
+	                  description: description,
+	                  amount: {
+	                      value: price
+	                  }
+	              }]
+	          });
+	      },
+	      onApprove: function (data, actions) {
+	          return actions.order.capture().then(function (details) {
+	          	  var firstname = $('#f_name').val();
+	          	  var middlename = $('#m_name').val();
+	          	  var lastname = $('#l_name').val();
+	          	  var email = $('#email').val();
+	          	  var password = $('#password').val();
+	          	  var address = $('#address').val();
+	              alert('Transaction completed by ' + details.payer.name.given_name);
+	              $.ajax({
+	              	url: 'authors/insertAuthorByJson?description='+description+'&price='+price+'&fname='+firstname+'&mname='+middlename+'&lname='+lastname+'&email='+email+'&password='+password+'&address='+address,
+	              	method: 'GET',
+	              	data: '',
+	              	dataType: 'json',
+	              	success: function(response) {
+	              		alert(response.inputs);
+	              	}
+	              });
+	          });
+	      },
+	      onError: (err) => {
+	        console.error(
+	          'An error prevented the buyer from checking out with PayPal',
+	        )
+	      },
+	  }).render('#paypal-button-container');
+	</script>
+
+    </body>
+</html>
