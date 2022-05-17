f7c1174a5fc363e53181c4c8b146b0d5fd178671
kenneth <kenneth.rygeltech@gmail.com>
2022-05-17 18:20:36 +0800

    Update Service COnstruct Method by adding restriction on subscription expired page

--- application/controllers/Services.php
+++ application/controllers/Services.php
@@ -5,12 +5,14 @@
         	$this->load->library(['ion_auth', 'form_validation']);
         	$this->load->model('service_model');
             $this->load->model('author_model');
-            $user = $this->ion_auth->get_user_id();
-            $author_details = $this->author_model->getAuthorbyIonId($user);
-            $now = gmdate('Y-m-d H:i:s');
-            if (!$this->ion_auth->logged_in()) {
-                if ($now >= $author_details->expired_at) {
-                    redirect('subscription/expired');
+            if ($this->ion_auth->in_group(array('author'))) {
+                $user = $this->ion_auth->get_user_id();
+                $author_details = $this->author_model->getAuthorbyIonId($user);
+                $now = gmdate('Y-m-d H:i:s');
+                if (!$this->ion_auth->logged_in()) {
+                    if ($now >= $author_details->expired_at) {
+                        redirect('subscription/expired');
+                    }
                 }
             }
         }
